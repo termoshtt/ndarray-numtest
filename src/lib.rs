c427000f9b776c00802f115e633d06724ca4294e
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-12-03 16:23:19 +0900

    Initializer for complex random array

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ extern crate ndarray;
 extern crate ndarray_rand;
 extern crate float_cmp;
 extern crate num_traits;
+extern crate num_complex;
 
 pub mod prelude;
 pub mod assert;
--- src/random.rs
+++ src/random.rs
@@ -4,19 +4,34 @@ use rand::Rng;
 use rand::distributions::*;
 use ndarray::{DataOwned, Dimension, ArrayBase, ShapeBuilder};
 use ndarray_rand::RandomExt;
+use num_complex::Complex;
+use num_traits::Num;
 
-pub trait RandomExtAppend<S, D>: RandomExt<S, D>
+pub trait RandomExtNormal<S, D>: RandomExt<S, D>
     where S: DataOwned,
           D: Dimension
 {
     fn random_normal<Sh>(shape: Sh, mean: S::Elem, variance: S::Elem) -> ArrayBase<S, D> where Sh: ShapeBuilder<Dim = D>;
+}
+
+pub trait RandomExtNormalComplex<A, S, D>: RandomExt<S, D>
+    where S: DataOwned<Elem = Complex<A>>,
+          D: Dimension
+{
+    fn random_normal_complex<Sh>(shape: Sh, mean: A, variance: A) -> ArrayBase<S, D> where Sh: ShapeBuilder<Dim = D>;
+}
+
+pub trait RandomExtRange<S, D>: RandomExt<S, D>
+    where S: DataOwned,
+          D: Dimension
+{
     fn random_range<Sh, IdS>(shape: Sh, min: S::Elem, ma: S::Elem) -> ArrayBase<S, D> where Sh: ShapeBuilder<Dim = D>;
 }
 
-impl<A, S, D> RandomExtAppend<S, D> for ArrayBase<S, D>
+impl<A, S, D> RandomExtNormal<S, D> for ArrayBase<S, D>
     where S: DataOwned<Elem = A>,
           D: Dimension,
-          A: From<f64> + Into<f64> + range::SampleRange + PartialOrd
+          A: From<f64> + Into<f64>
 {
     fn random_normal<Sh>(shape: Sh, mean: A, variance: A) -> ArrayBase<S, D>
         where Sh: ShapeBuilder<Dim = D>
@@ -24,6 +39,26 @@ impl<A, S, D> RandomExtAppend<S, D> for ArrayBase<S, D>
         let dist = NormalAny::new(mean, variance);
         Self::random(shape, dist)
     }
+}
+
+impl<A, S, D> RandomExtNormalComplex<A, S, D> for ArrayBase<S, D>
+    where S: DataOwned<Elem = Complex<A>>,
+          D: Dimension,
+          A: From<f64> + Into<f64> + Clone + Num
+{
+    fn random_normal_complex<Sh>(shape: Sh, mean: A, variance: A) -> ArrayBase<S, D>
+        where Sh: ShapeBuilder<Dim = D>
+    {
+        let dist = ComplexNormal::new(mean, variance);
+        Self::random(shape, dist)
+    }
+}
+
+impl<A, S, D> RandomExtRange<S, D> for ArrayBase<S, D>
+    where S: DataOwned<Elem = A>,
+          D: Dimension,
+          A: range::SampleRange + PartialOrd
+{
     fn random_range<Sh, IdS>(shape: Sh, min: A, max: A) -> ArrayBase<S, D>
         where Sh: ShapeBuilder<Dim = D>
     {
@@ -32,7 +67,7 @@ impl<A, S, D> RandomExtAppend<S, D> for ArrayBase<S, D>
     }
 }
 
-pub struct NormalAny<A: From<f64> + Into<f64>> {
+pub struct NormalAny<A> {
     dist: Normal,
     phantom: PhantomData<A>,
 }
@@ -61,5 +96,43 @@ impl<A: From<f64> + Into<f64>> IndependentSample<A> for NormalAny<A>
         where R: Rng
     {
         self.dist.ind_sample(rng).into()
+    }
+}
+
+pub struct ComplexNormal<A> {
+    dist: Normal,
+    phantom: PhantomData<A>,
+}
+
+impl<A: Into<f64>> ComplexNormal<A> {
+    pub fn new(center: A, var: A) -> Self {
+        ComplexNormal {
+            dist: Normal::new(center.into(), var.into()),
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl<A> Sample<Complex<A>> for ComplexNormal<A>
+    where A: Clone + Num + From<f64>
+{
+    fn sample<R>(&mut self, rng: &mut R) -> Complex<A>
+        where R: Rng
+    {
+        let re = self.dist.sample(rng).into();
+        let im = self.dist.sample(rng).into();
+        Complex::new(re, im)
+    }
+}
+
+impl<A> IndependentSample<Complex<A>> for ComplexNormal<A>
+    where A: Clone + Num + From<f64>
+{
+    fn ind_sample<R>(&self, rng: &mut R) -> Complex<A>
+        where R: Rng
+    {
+        let re = self.dist.ind_sample(rng).into();
+        let im = self.dist.ind_sample(rng).into();
+        Complex::new(re, im)
     }
 }
