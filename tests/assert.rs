8eed6c62f857e456f5a4979d14904205f7054bde
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-12-04 16:58:52 +0900

    Add test for complex array

--- tests/assert.rs
+++ tests/assert.rs
@@ -1,9 +1,14 @@
 
 extern crate ndarray;
+extern crate num;
 extern crate ndarray_numtest;
 
 use ndarray::prelude::*;
 use ndarray_numtest::prelude::*;
+use num::complex::Complex;
+
+#[allow(non_camel_case_types)]
+type c64 = Complex<f64>;
 
 #[test]
 fn allclose_success() {
@@ -17,5 +22,20 @@ fn allclose_success() {
 fn allclose_fail() {
     let a = arr1(&[1.0, 2.0]);
     let b = arr1(&[1.0, 2.0 + 1.0e-3]);
+    b.assert_allclose(&a, 1e-7);
+}
+
+#[test]
+fn allclose_success_complex() {
+    let a = arr1(&[c64::new(1.0, 1.1), c64::new(1.0, 1.1)]);
+    let b = arr1(&[c64::new(1.0, 1.1 + 1.0e-9), c64::new(1.0, 1.1)]);
+    b.assert_allclose(&a, 1e-7);
+}
+
+#[should_panic]
+#[test]
+fn allclose_success_fail() {
+    let a = arr1(&[c64::new(1.0, 1.1), c64::new(1.0, 1.1)]);
+    let b = arr1(&[c64::new(1.0, 1.1 + 1.0e-3), c64::new(1.0, 1.1)]);
     b.assert_allclose(&a, 1e-7);
 }
